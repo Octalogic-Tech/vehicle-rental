be431d2b08d1b41f04548f6f7f993e8d0ea84ef3
clintonsoares <soares.clinton@gmail.com>
2019-01-21 14:53:39 +0530

    Changed the custom path for customer vehicles list

--- application/views/api/vr_api_view.php
+++ application/views/api/vr_api_view.php
@@ -1,40 +1,6 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-
-
-
-
-	// foreach($array as $row)
-	// {
-	// 	$arr['id'] = (int)$row->id;
-	// 	$arr['name'] = $row->name;
-	// 	$arr['brand'] = $row->brand;
-	// 	$arr['type'] = $row->type;
-	// 	$arr['categories'] = $row->categories;
-	// 	$arr['firstname'] = $row->firstname;
-	// 	$arr['lastname'] = $row->lastname;
-	// 	$arr['place'] = $row->place;
-	// 	$arr['colorName'] = $row->colorName;
-
-	// 	$my_values = array($arr);
-
-
-	// 	// $json = json_encode($my_values, JSON_PRETTY_PRINT);
-
-	// 	// echo '<pre>';
-	// 	// echo($json);
-	// 	// echo "</pre>";
-
-
-	// }
-
-	// 	$json = json_encode($my_values, JSON_PRETTY_PRINT);
-
-		echo '<pre>';
 		echo($json);
-		echo "</pre>";
 
-	
 ?>
